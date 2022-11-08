241ec802103738741ff4ebcb0090c3962ff18391
ShivaniPatid <shivanipatidar712@gmail.com>
2022-11-08 20:27:34 +0530

    User Registration

---
+++ FundooNotes/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using BussinessLayer.Interfaces;
+using CommonLayer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FundooNotes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserBL userBL;
+
+        public UserController(IUserBL userBL)
+        {
+            this.userBL = userBL;
+        }
+
+        [HttpPost("Register")]
+        public IActionResult AddUser(UserRegistration userRegistration)
+        {
+            try
+            {
+                var result = this.userBL.Registration(userRegistration);
+                if (result != null)
+                {
+                    return this.Ok(new { success = true, message = "Registration succsessfull", data=result });
+                }
+                else
+                {
+                    return this.BadRequest(new { success = false, message = "Registration Unsuccsessfull" });
+
+                }
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
