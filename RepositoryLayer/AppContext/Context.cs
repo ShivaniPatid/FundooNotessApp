415f0cadab4a388bdbc2e2d7498aa59562a02a3d
ShivaniPatid <shivanipatidar712@gmail.com>
2022-11-08 20:34:28 +0530

    Added label entity

---
+++ RepositoryLayer/AppContext/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Entities;
+
+namespace RepositoryLayer.AppContext
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+    }
+}
