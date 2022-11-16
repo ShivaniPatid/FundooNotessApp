d08797ba5319b436bbc12dda714b513f2da504e3
ShivaniPatid <shivanipatidar712@gmail.com>
2022-11-16 17:52:39 +0530

    Implemented redis cache in CollabController

--- FundooNotes/Controllers/CollabController.cs
+++ FundooNotes/Controllers/CollabController.cs
@@ -6,6 +6,14 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RepositoryLayer.Entities;
 using System.Collections.Generic;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using RepositoryLayer.AppContext;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.EntityFrameworkCore;
+using BussinessLayer.Services;
 
 namespace FundooNotes.Controllers
 {
@@ -15,10 +23,16 @@ namespace FundooNotes.Controllers
     public class CollabController : ControllerBase
     {
         private readonly ICollabBL collabBL;
+        private readonly IMemoryCache memoryCache;
+        private readonly Context context;
+        private readonly IDistributedCache distributedCache;
 
-        public CollabController(ICollabBL collabBL)
+        public CollabController(ICollabBL collabBL, IMemoryCache memoryCache, Context context, IDistributedCache distributedCache)
         {
             this.collabBL = collabBL;
+            this.memoryCache = memoryCache;
+            this.context = context;
+            this.distributedCache = distributedCache;
         }
 
         [HttpPost("Add")]
@@ -43,19 +57,6 @@ namespace FundooNotes.Controllers
             }
         }
 
-        [HttpGet("View")]
-        public IEnumerable<CollabEntity> GetCollaboratorsByNoteId(long noteId)
-        {
-            try
-            {
-                var userId = Convert.ToInt32(User.Claims.FirstOrDefault(e => e.Type == "Id").Value);
-                return collabBL.GetCollaboratorsByNoteId(userId, noteId);  
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
 
         [HttpDelete("Remove")]
         public IActionResult RemoveCollaborator(long noteId, long collabId)
@@ -78,6 +79,46 @@ namespace FundooNotes.Controllers
 
                 throw;
             }
+        }
+
+        [HttpGet("View")]
+        public IEnumerable<CollabEntity> GetCollaboratorsByNoteId(long noteId)
+        {
+            try
+            {
+                var userId = Convert.ToInt32(User.Claims.FirstOrDefault(e => e.Type == "Id").Value);
+                return collabBL.GetCollaboratorsByNoteId(userId, noteId);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet("RedisCache")]
+        public async Task<IActionResult> GetAllCollaUsingRedisCache()
+        {
+            var cacheKey = "collabList";
+            string serializedCollabList;
+            var collabList = new List<CollabEntity>();
+            var redisCollabList = await distributedCache.GetAsync(cacheKey);
+            if (redisCollabList != null)
+            {
+                serializedCollabList = Encoding.UTF8.GetString(redisCollabList);
+                collabList = JsonConvert.DeserializeObject<List<CollabEntity>>(serializedCollabList);
+            }
+            else
+            {
+                collabList = await context.Collaborator.ToListAsync();
+                //noteList = (List<NoteEntity>)noteBL.GetAllNotes();
+                serializedCollabList = JsonConvert.SerializeObject(collabList);
+                redisCollabList = Encoding.UTF8.GetBytes(serializedCollabList);
+                var options = new DistributedCacheEntryOptions()
+                    .SetAbsoluteExpiration(DateTime.Now.AddMinutes(10))
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(2));
+                await distributedCache.SetAsync(cacheKey, redisCollabList, options);
+            }
+            return Ok(collabList);
         }
     }
 }
